b8baebc3d74c4e1af335962f52cbed416948346c
Dan Reist <djreist@outlook.com>
2018-10-11 20:32:03 -0400

    Split the PSCredential into multiple files

---
+++ Functions/Import-PSCredential.ps1
@@ -0,0 +1,127 @@
+<#
+    .SYNOPSIS
+        Retrieves and decrypts an exported PowerShell Credential from file or Azure KeyVault back into a PSCredential object.
+
+    .DESCRIPTION
+        Import-PSCredential is used to retrieve a previously saved PowerShell Credential object from disk
+        or from Azure KeyVault and returns a PowerShell Credential object [System.Management.Automation.PSCredential] that can be used within scripts and
+        Desired State Configurations. When retrieving from disk, the method used to encrypt the credential must be used to decrypt the credential.
+
+    .PARAMETER Path
+        Path to the JSON file that contains the encrypted credential.
+
+    .PARAMETER SecureKey
+        The SecureString that was used as a Pre-Shared-Key for encrypting the credential password.
+
+    .PARAMETER Thumbprint
+        The Thumbprint of the certificate on the local computer that contains the private key of the certificate used to encrypt the credential password.
+
+    .PARAMETER CertificateStore
+        Specifies the certifcate store of the specified certificate thumbprint. Either LocalMachine or CurrentUser.
+
+    .PARAMETER KeyVault
+        The name of the Azure KeyVault that will that contains the exported credential secret.
+
+    .PARAMETER SecretName
+        The name of the Azure KeyVault secret that contains the exported credential.
+
+    .EXAMPLE
+        Import a credential from file using a pre-shared key.
+        $Credential = Import-PSCredential -Path ./savedcredential.json -SecureKey ('$ecretK3y' | Convertto-SecureString -AsPlainText -Force)
+
+    .EXAMPLE
+        Import a credential from file using a Certificate
+        $Credential = Import-PSCredential -Path ./savedcredential.json -Thumbprint '87BB70A19A7671D389F49AF4C9608B2F381FDD80'
+
+    .EXAMPLE
+        Import a credential from an Azure KeyVault. The user executing the script must be authenticated to Azure with sufficient permissions to the KeyVault.
+        $Credential = Import-PSCredential -KeyVault 'My-KeyVault' -SecretName 'SavedCred-Secret'
+#>
+function Import-PSCredential
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true, ParameterSetName="LocalKey",Position=1)]
+        [Parameter(Mandatory = $true, ParameterSetName="CertificateThumbprint",Position=1)]
+        [System.String]
+        $Path,
+
+        [Parameter(Mandatory = $true, ParameterSetName="LocalKey")]
+        [System.Security.SecureString]
+        $SecureKey,
+
+        [Parameter(Mandatory = $true, ParameterSetName="CertificateThumbprint")]
+        [ValidateNotNullorEmpty()]
+        [System.String]$Thumbprint,
+
+        [Parameter(Mandatory = $false, ParameterSetName="CertificateThumbprint")]
+        [ValidateSet("LocalMachine","CurrentUser")]
+        [System.String]
+        $CertificateStore,
+
+        [Parameter(Mandatory = $true, ParameterSetName="KeyVault")]
+        [System.String]
+        $KeyVault,
+
+        [Parameter(Mandatory = $true, ParameterSetName="KeyVault")]
+        [System.String]
+        $SecretName
+    )
+
+
+    if ($PSCmdlet.ParameterSetName -eq 'KeyVault')
+    {
+        try
+        {
+            $keyVaultObject = Get-AzureRmKeyVault -VaultName $keyVault -ErrorAction Stop
+        }
+        catch
+        {
+            throw "Unable to access KeyVault $KeyVault, ensure that the current session has access to it. Use Add-AzureRmAccount or Login-AzureRmAccount to establish access for the current session. $($_)"
+        }
+
+        if ($null -eq $keyVaultObject)
+        {
+            throw "Unable to find KeyVault $KeyVault within the current subscription"
+        }
+
+        Write-Verbose -Message "Reading credential object data from $KeyVault"
+        $SecretData = Get-AzureKeyVaultSecret -VaultName $KeyVault -Name $SecretName
+
+        if ($null -eq $SecretData)
+        {
+            throw "Could not read data from $SecretName in $KeyVault"
+        }
+
+        $username = ($SecretData.Attributes.Tags).username
+        if ($null -eq $username)
+        {
+            throw "$SecretName does not have a Username tag"
+        }
+
+        New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList @($username,$SecretData.SecretValue)
+
+    }
+    elseif ($PSBoundParameters.ContainsKey('Path'))
+    {
+        Write-Verbose "Reading credential object data from $Path"
+        $CredentialImport = Get-Content $Path -Raw | ConvertFrom-Json
+
+        if ($PSCmdlet.ParameterSetName -eq 'LocalKey')
+        {
+            $SecureStringPassword = ConvertTo-FIPSsecureString -EncryptedString $CredentialImport.Password -SecureKey $SecureKey -Verbose:$Verbose
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq 'CertificateThumbprint')
+        {
+            if ($PSBoundParameters.ContainsKey('CertificateStore'))
+            {
+                $SecureStringPassword = ConvertTo-PKISecureString -EncryptedString $CredentialImport.Password -Thumbprint $Thumbprint -CertificateStore $CertificateStore
+            }
+
+            $SecureStringPassword = ConvertTo-PKISecureString -EncryptedString $CredentialImport.Password -Thumbprint $Thumbprint
+        }
+
+        New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList @(($CredentialImport.UserName),$SecureStringPassword)
+    }
+}
---
+++ Functions/New-PSCredential.ps1
@@ -0,0 +1,96 @@
+<#
+    .SYNOPSIS
+        Create a new PowerShell Credential object with a random password. Encrypts and saves the Credential object to file or to Azure KeyVault
+
+    .DESCRIPTION
+        New-PSCredential is used to create a new  PowerShell Credential object [System.Management.Automation.PSCredential] with the provided username and
+        a strong random password. The resulting credential object is returned as well as saved to disk or to Azure KeyVault so that it can be retrieved later.
+        When saving to disk, the password is encrypted with either a pre-shared key or PKI certificate.
+
+    .PARAMETER Username
+        Username to use for the Credential to be created.
+
+    .PARAMETER Path
+        Path to the JSON file that will be created to save the encrypted credential.
+
+    .PARAMETER SecureKey
+        A SecureString that is used as a Pre-Shared-Key for encrypting the credential password.
+
+    .PARAMETER Thumbprint
+        The ThumbPrint of a certificate on the local computer that will be used to encrypt the credential password.
+
+    .PARAMETER CertificateFile
+        Path to a .CER certificate public key file that will be used to encrypt the credential password.
+
+    .PARAMETER CertificateStore
+        Specifies the certifcate store of the specified certificate thumbprint. Either LocalMachine or CurrentUser.
+
+    .PARAMETER KeyVault
+        The name of the Azure KeyVault that will be used to store the exported credential.
+
+    .PARAMETER SecretName
+        The name of the Azure KeyVault secret to create that will be used to store the exported credential.
+
+    .EXAMPLE
+        Creating a credential to be used as a service account, and creating the account.
+        $Credential = New-PSCredential -Username 'svc.SharePoint.farm' -Path ./savedcredential.json -Thumbprint '87BB70A19A7671D389F49AF4C9608B2F381FDD80'
+        New-ADUser -Name $Credential.Username -AccountPassword $Credential.Password -Enabled:$true
+#>
+function New-PSCredential
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true, ParameterSetName="LocalKey",Position=1,ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ParameterSetName="KeyVault",Position=1,ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ParameterSetName="CertificateThumbprint",Position=1,ValueFromPipeline=$true)]
+        [Parameter(Mandatory = $true, ParameterSetName="CertFile",Position=1,ValueFromPipeline=$true)]
+        [System.String]
+        $Username,
+
+        [Parameter(Mandatory = $true, ParameterSetName="LocalKey",Position=2)]
+        [Parameter(Mandatory = $true, ParameterSetName="CertificateThumbprint",Position=2)]
+        [Parameter(Mandatory = $true, ParameterSetName="CertFile",Position=2)]
+        [ValidateNotNullorEmpty()]
+        [System.String]
+        $Path,
+
+        [Parameter(Mandatory = $true, ParameterSetName="LocalKey")]
+        [ValidateNotNullorEmpty()]
+        [System.Security.SecureString]
+        $SecureKey,
+
+        [Parameter(Mandatory = $true, ParameterSetName="CertificateThumbprint")]
+        [ValidateNotNullorEmpty()]
+        [System.String]
+        $Thumbprint,
+
+        [Parameter(Mandatory = $true, ParameterSetName="CertFile",Position=1,ValueFromPipeline=$true)]
+        [ValidateScript({test-path $_})]
+        [System.String]
+        $CertificateFile,
+
+        [Parameter(Mandatory = $false, ParameterSetName="CertificateThumbprint")]
+        [ValidateSet("LocalMachine","CurrentUser")]
+        [System.String]
+        $CertificateStore,
+
+        [Parameter(Mandatory = $true, ParameterSetName="KeyVault")]
+        [System.String]
+        $KeyVault,
+
+        [Parameter(Mandatory = $true, ParameterSetName="KeyVault")]
+        [System.String]
+        $SecretName
+    )
+
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList @($Username, (ConvertTo-SecureString -String (New-Password) -AsPlainText -Force) )
+
+    $PSBoundParameters.Remove('Username') | Out-Null
+    $ExportParameters = $PSBoundParameters
+    $ExportParameters.Add('Credential',$Credential)
+
+    Export-PSCredential @ExportParameters | Out-Null
+
+    return $Credential
+}
